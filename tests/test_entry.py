367b4e38da741c76c8f7dbe385fc5d2c2e0737b1
Nagidal <nagidal146@gmail.com>
2020-08-04 21:08:29 +0200

    Writing tests for config file

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -13,6 +13,7 @@ cfg_path = entry.provide_user_dir(pathlib.Path(entry.dir_name)) / entry.ini_name
 def test_config_file_creation():
     with config_loader.Config(cfg_path) as config:
         assert config.path_to_config_file.exists()
+        assert type(config.playfield_width) is int
 
 def test_config_file_deletion():
     assert not cfg_path.exists()
